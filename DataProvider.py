3c6c04f2c07b291350fb4c5557d7c3c17db2bed5
Filip Guzy <filip.m.guzy@gmail.com>
2021-04-27 22:22:50 +0200

    Update DataProvider

--- DataProvider.py
+++ DataProvider.py
@@ -2,7 +2,25 @@ from FashionMnistDataset import *
 from torch.utils.data import DataLoader, SubsetRandomSampler
 
 
-def get_fashion_mnist_dataloaders(batch_size=128):
+def get_celeba_dataloaders(self, batch_size=128):
+    dataset = CelebADataset().load_dataset()
+    dataloaders = [
+        DataLoader(dataset=dataset, batch_size=batch_size, shuffle=False, drop_last=True,
+                   sampler=SubsetRandomSampler(indices=list(range(143258)))),
+        DataLoader(dataset=dataset, batch_size=batch_size, shuffle=False, drop_last=True,
+                   sampler=SubsetRandomSampler(indices=list(range(70000, 200000)))),
+        DataLoader(dataset=dataset, batch_size=batch_size, shuffle=False, drop_last=True,
+                   sampler=SubsetRandomSampler(indices=list(range(143258, 277006)))),
+        DataLoader(dataset=dataset, batch_size=batch_size, shuffle=False, drop_last=True,
+                   sampler=SubsetRandomSampler(indices=list(range(70000, 200000)))),
+        DataLoader(dataset=dataset, batch_size=batch_size, shuffle=False, drop_last=True,
+                   sampler=SubsetRandomSampler(indices=list(range(143258)))),
+    ]
+
+    return dataloaders
+
+
+def get_fashion_mnist_dataloaders(self, batch_size=128):
     dataset = FashionMnistDataset().load_dataset()
     dataloaders = [
         DataLoader(dataset=dataset, batch_size=batch_size, shuffle=False, drop_last=True,
@@ -13,5 +31,20 @@ def get_fashion_mnist_dataloaders(batch_size=128):
                    sampler=SubsetRandomSampler(indices=list(range(90000)))),
         DataLoader(dataset=dataset, batch_size=batch_size, shuffle=False, drop_last=True,
                    sampler=SubsetRandomSampler(indices=list(range(90000, 180000))))
+    ]
+    return dataloaders
+
+
+def get_cifar10_dataloaders(self, batch_size=128):
+    dataset = Cifar10Dataset().load_dataset()
+    dataloaders = [
+        DataLoader(dataset=dataset, batch_size=batch_size, shuffle=False, drop_last=True,
+                   sampler=SubsetRandomSampler(indices=list(range(250000, 500000)))),
+        DataLoader(dataset=dataset, batch_size=batch_size, shuffle=False, drop_last=True,
+                   sampler=SubsetRandomSampler(indices=list(range(250000)))),
+        DataLoader(dataset=dataset, batch_size=batch_size, shuffle=False, drop_last=True,
+                   sampler=SubsetRandomSampler(indices=list(range(250000, 500000)))),
+        DataLoader(dataset=dataset, batch_size=batch_size, shuffle=False, drop_last=True,
+                   sampler=SubsetRandomSampler(indices=list(range(250000))))
     ]
     return dataloaders

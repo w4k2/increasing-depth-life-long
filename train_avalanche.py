5d23722ec5920e51794b427bcb3d8a79a580c29d
jedrzejkozal <jedrzej.kozal@gmail.com>
2022-02-09 15:46:35 +0100

    add LwF method and hyperparamter config

--- train_avalanche.py
+++ train_avalanche.py
@@ -15,7 +15,7 @@ from avalanche.benchmarks.datasets import MNIST, FashionMNIST, CIFAR10
 from avalanche.benchmarks.generators import dataset_benchmark
 from avalanche.benchmarks.classic import PermutedMNIST, SplitCIFAR100, SplitMNIST, SplitCIFAR10, SplitTinyImageNet, CORe50
 from avalanche.evaluation.metrics.confusion_matrix import StreamConfusionMatrix
-from avalanche.training.strategies import BaseStrategy, EWC, GEM, Replay, SynapticIntelligence, Cumulative
+from avalanche.training.strategies import BaseStrategy, EWC, GEM, Replay, SynapticIntelligence, Cumulative, LwF
 from avalanche.models import SimpleMLP
 from utils.mlflow_logger import MLFlowLogger
 from avalanche.training.plugins import EvaluationPlugin
@@ -84,7 +84,7 @@ def parse_args():
     parser.add_argument('--experiment', default='Default', help='flow experiment name')
     parser.add_argument('--nested_run', action='store_true', help='create nested run in mlflow')
 
-    parser.add_argument('--method', default='agem', choices=('baseline', 'cumulative', 'll-stochastic-depth', 'ewc', 'si', 'gem', 'agem', 'pnn', 'replay'))
+    parser.add_argument('--method', default='agem', choices=('baseline', 'cumulative', 'll-stochastic-depth', 'ewc', 'si', 'gem', 'agem', 'pnn', 'replay', 'lwf'))
     parser.add_argument('--base_model', default='resnet18', choices=('resnet9', 'resnet18', 'resnet50', 'resnet18-stoch', 'resnet50-stoch', 'vgg', 'simpleMLP'))
     parser.add_argument('--pretrained', default=True, type=distutils.util.strtobool, help='if True load weights pretrained on imagenet')
     parser.add_argument('--dataset', default='permutation-mnist', choices=('cifar100', 'cifar10', 'mnist', 'permutation-mnist', 'tiny-imagenet', 'cifar10-mnist-fashion-mnist', 'cores50'))
@@ -267,7 +267,8 @@ def get_method(args, device, classes_per_task, use_mlflow=True):
         criterion = nn.CrossEntropyLoss()
         strategy = CumulativeModified(model, optimizer, criterion, 
                                 train_mb_size=args.batch_size, eval_mb_size=args.batch_size,
-                                device=device, train_epochs=args.n_epochs, plugins=plugins, evaluator=evaluation_plugin
+                                device=device, train_epochs=args.n_epochs, plugins=plugins, 
+                                evaluator=evaluation_plugin, eval_every=-1
                                 )
 
     elif args.method == 'll-stochastic-depth':
@@ -316,6 +317,13 @@ def get_method(args, device, classes_per_task, use_mlflow=True):
         optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
         criterion = nn.CrossEntropyLoss()
         strategy = ReplayModified(model, optimizer, criterion, mem_size=250*args.n_experiences,
+                                  train_mb_size=args.batch_size, eval_mb_size=args.batch_size, device=device,
+                                  train_epochs=args.n_epochs, evaluator=evaluation_plugin, eval_every=-1)
+    elif args.method == 'lwf':
+        model = resnet.resnet18_multihead(num_classes=classes_per_task[0], input_channels=input_channels, pretrained=args.pretrained)
+        optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.9, weight_decay=args.weight_decay)
+        criterion = nn.CrossEntropyLoss()
+        strategy = LwF(model, optimizer, criterion, alpha=1.0, temperature=1.0,
                                   train_mb_size=args.batch_size, eval_mb_size=args.batch_size, device=device,
                                   train_epochs=args.n_epochs, evaluator=evaluation_plugin, eval_every=-1)
 

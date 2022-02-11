b628560649a4730983f7fd76ecd70411798f0e20
jedrzejkozal <jedrzej.kozal@gmail.com>
2022-02-11 20:55:38 +0100

    add multi-datasets benchmarks

---
+++ multi_replay.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+python train_avalanche.py --run_name="test" --experiment="Multi" --method="replay" --dataset="cifar10-mnist-fashion-mnist" --n_experiences=20 --train_on_experiences=20 --device="cuda:1" --batch_size=128 --num_workers=10 --seed=43 --n_epochs=10 --lr=0.01 --weight_decay=1e-05
+python train_avalanche.py --run_name="test" --experiment="Multi" --method="replay" --dataset="mnist-fashion-mnist-cifar10" --n_experiences=20 --train_on_experiences=20 --device="cuda:1" --batch_size=128 --num_workers=10 --seed=43 --n_epochs=10 --lr=0.01 --weight_decay=1e-05
+python train_avalanche.py --run_name="test" --experiment="Multi" --method="replay" --dataset="fashion-mnist-cifar10-mnist" --n_experiences=20 --train_on_experiences=20 --device="cuda:1" --batch_size=128 --num_workers=10 --seed=43 --n_epochs=10 --lr=0.01 --weight_decay=1e-05
+
+python train_avalanche.py --run_name="test" --experiment="Multi" --method="replay" --dataset="cifar10-mnist-fashion-mnist" --n_experiences=20 --train_on_experiences=20 --device="cuda:1" --batch_size=128 --num_workers=10 --seed=43 --n_epochs=20 --lr=0.01 --weight_decay=1e-05
+python train_avalanche.py --run_name="test" --experiment="Multi" --method="replay" --dataset="mnist-fashion-mnist-cifar10" --n_experiences=20 --train_on_experiences=20 --device="cuda:1" --batch_size=128 --num_workers=10 --seed=43 --n_epochs=20 --lr=0.01 --weight_decay=1e-05
+python train_avalanche.py --run_name="test" --experiment="Multi" --method="replay" --dataset="fashion-mnist-cifar10-mnist" --n_experiences=20 --train_on_experiences=20 --device="cuda:1" --batch_size=128 --num_workers=10 --seed=43 --n_epochs=20 --lr=0.01 --weight_decay=1e-05

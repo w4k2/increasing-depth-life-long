4e486cb49a5d6d8f153328fc0e90c78e363b538f
jedrzejkozal <jedrzej.kozal@gmail.com>
2022-02-22 15:47:59 +0100

    fix number of training expiriences with final run

--- hyperparamters.py
+++ hyperparamters.py
@@ -23,6 +23,7 @@ def main():
 
     n_repeats = 5
     args = select_best_paramters(args, client, experiment, parrent_run_id)
+    args.train_on_experiences = args.n_experiences
 
     with mlflow.start_run(experiment_id=experiment_id, run_name=f'{args.method} final'):
         for repeat in range(n_repeats):
